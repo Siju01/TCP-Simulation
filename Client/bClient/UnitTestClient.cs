246c64258f6760dc66f63b2d178d21e7fc0efb07
Siju <63763376+Siju01@users.noreply.github.com>
2021-12-22 09:09:29 +0700

    Revision

---
+++ Client/bClient/UnitTestClient.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Runtime.Serialization.Formatters.Binary;
+using Client;
+
+[TestClass]
+public class UnitTest
+{
+    [TestMethod]
+    public void Check_Test()
+    {
+        CheckM cm = new CheckM();
+        Assert.Equal(1, cm.Check());
+    }
+}
+
+
+[TestMethod]
+public void Main_Test()
+{
+    Main m = new Main();
+    Assert.Equal(1, m.Main());
+}
